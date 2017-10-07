44ef293b6aad43876bee77042f6fb033c02f0151
saraen7 <sara.e.norlin@hotmail.com>
2017-10-07 16:16:33 -0400

    Complete through Chapter 5
    Examples that I could not solve are noted with the author's solution and my explanation comments

--- eloquentJavascriptExercise.js
+++ eloquentJavascriptExercise.js
@@ -1,11 +1,10 @@
 //Eloquent JavaScript exercises
 
-//Chapter 2
-//Exercise 1
+//Chapter 2, Exercise 1
 for (var line = "#"; line.length < 8; line += "#")
   console.log(line);
 
-//Exercise 2
+//Chapter 2, Exercise 2
 for (var i = 1; i <= 100; i++) {
   if(i%5==0 && i%3==0){
     console.log("FizzBuzz")
@@ -18,7 +17,7 @@ for (var i = 1; i <= 100; i++) {
   }
 }
 
-//chapter 2, example 3
+//Chapter 2, Exercise 3
 var size = 8;
 
 var chessBoard = "";
@@ -47,9 +46,7 @@ for (var i = 0; i <= size; i++){
 
 console.log(chessBoard) //new line
 
-
-//Chapter 3
-//Exericse 1
+//Chapter 3, Exericse 1
 var min = function(a, b){
   if(a < b){
     console.log(a);
@@ -62,7 +59,7 @@ var min = function(a, b){
 
 min(4,5); //example
 
-//Exercise 2
+//Chapter 3, Exercise 2
 //note this works on negative numbers too even though in the example it's not supposed to
 var even = function(a) {
   var toText = a.toString();
@@ -77,7 +74,7 @@ var even = function(a) {
   }
 }
 
-//Exercise 3, part 1
+//Chapter 3, Exercise 3, part 1
 var countBs = function(a) {
   a = a.toString();
   var b = 0;
@@ -90,7 +87,7 @@ var countBs = function(a) {
 }
 countBs("BBC");
 
-//Exercise 3, part 2
+//Chapter 3, Exercise 3, part 2
 var countChar = function(a,c) {
   a = a.toString();
   c = c.toString();
@@ -104,8 +101,7 @@ var countChar = function(a,c) {
 }
 countChar("kakkerlak", "k");
 
-//Chapter 4
-//Exercise 1, part 1
+//Chapter 4, Exercise 1, part 1
 var range = function(start,end){
   var numbers = [];
   for(i = start; i <= end; i++){
@@ -116,7 +112,7 @@ var range = function(start,end){
 
 console.log(range(2,9))
 
-//Exercise 1, part 2
+//Chapter 4, Exercise 1, part 2
 var sum = function(array){
   var total = 0;
   for(var i = 0; i < array.length; i++){
@@ -125,7 +121,7 @@ var sum = function(array){
   return total;
 }
 
-//Exercise 2, part 1
+//Chapter 4, Exercise 2, part 1
 var reverseArray = function(array){
   var newArray = [];
   newArray.push(array[0]); //adds the first item to the array
@@ -137,7 +133,7 @@ var reverseArray = function(array){
 
 console.log(reverseArray(["A", "B", "C"]));
 
-//Exercise 2, part 2
+//Chapter 4, Exercise 2, part 2
 var reverseArrayInPlace = function(array){
   for(var i = 0; i < Math.floor((array.length-1)/2); i++){
     var temp = array[i];
@@ -152,9 +148,222 @@ reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 
 //Chapter 4, Exercise 3, part 1
-var arrayToList = function(array){
-
+//Converts an array to a nested list
+function arrayToList(array){
+  var list = null;
+  for(var i = array.length-1; i >= 0; i--){ //more efficient to go backwards through the array to nest the list properly
+    list = {value: array[i], rest: list};
+  }
+  return list;
 }
 
-//Chapter 5
-//Exercise 1
+console.log(arrayToList([10, 20])); //to test
+
+//Chapter 4, Exercise 3, part 2
+//Converts a list to an array
+function listToArray(list){
+  var array = [];
+  for(var i = list; i; i = i.rest){
+    array.push(i);
+  }
+  return array;
+}
+
+console.log(listToArray(arrayToList([10, 20, 30]))); //to test in conjunction with exercise 3, part 1
+
+//Chater 4, Exercise 3, part 3
+function prepend(element,list){
+  var newList = {value: element, rest: list};
+  return newList;
+}
+
+console.log(prepend(10, prepend(20, null))); //to test
+
+//Chapter 4, Exercise 3, part 4
+function nth(list,number){
+  if(!list){ //if the value does not exist in the list
+    return undefined; //return undefined
+  } else if (number == 0){ //if the value of the list is 0
+    return list.value; //return the first position in the list
+  } else { //otherwise
+    return nth(list.rest, number - 1); //return the position in the list minus 1 (because arrays)
+  }
+}
+
+console.log(nth(arrayToList([10, 20, 30]), 1)); //for testing in conjunction with exercise 3, part 1
+
+//Chapter 4, Exercise 4, part 1 - does not work as of 10/7/17
+function deepEqual(val1,val2){
+  if(typeof val1 == "object" && typeof val2 == "object" && val1 != null && val2 != null){ //if val1 and val2 are both objects and not null
+    for(val1 in val2){ //see if val1 equals val2
+      return true;
+    } else {
+      return false;
+    }
+  } else if (val1 === val2){ //if val1 and val2 are not objects but are equal
+    return true;
+  } else { //if val1 and val2 are not the same
+    return false;
+  }
+}
+
+var obj = {here: {is: "an"}, object: 2}; //needed to test
+console.log(deepEqual(obj, obj)); //test for true
+console.log(deepEqual(obj, {here: 1, object: 2})); //test for false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2})); //test for true
+
+//Chapter 5, Exercise 1 - does not work as of 10/7/17
+var arrays = [[1, 2, 3], [4, 5], [6]]; //for testing
+
+console.log(reduce(arrays,function(arrays){
+  var combinedArray = [];
+  for(var i = 0; i < arrays.length-1; i++){
+    combinedArray.push(arrays[i])
+  }
+},0));
+
+//correct - from the author
+console.log(arrays.reduce(function(flat, current) {
+  return flat.concat(current);
+}, []));
+//I do not understand how the "start" can be [], why the function only takes two variables when there are three arrays in the variable array, ahhhhh
+
+//Chapter 5, Exercise 2
+//family information in JSON format from the author
+var ANCESTRY_FILE = JSON.stringify([
+  {"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"},
+  {"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"},
+  {"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"},
+  {"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"},
+  {"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"},
+  {"name": "Jan Frans van Brussel", "sex": "m", "born": 1761, "died": 1833, "father": "Jacobus Bernardus van Brussel", "mother":null},
+  {"name": "Pauwels van Haverbeke", "sex": "m", "born": 1535, "died": 1582, "father": "N. van Haverbeke", "mother":null},
+  {"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"},
+  {"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"},
+  {"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"},
+  {"name": "Pieter Haverbeke", "sex": "m", "born": 1602, "died": 1642, "father": "Lieven van Haverbeke", "mother":null},
+  {"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"},
+  {"name": "Pieter Bernard Haverbeke", "sex": "m", "born": 1695, "died": 1762, "father": "Willem Haverbeke", "mother": "Petronella Wauters"},
+  {"name": "Lieven van Haverbeke", "sex": "m", "born": 1570, "died": 1636, "father": "Pauwels van Haverbeke", "mother": "Lievijne Jans"},
+  {"name": "Joanna de Causmaecker", "sex": "f", "born": 1762, "died": 1807, "father": "Bernardus de Causmaecker", "mother":null},
+  {"name": "Willem Haverbeke", "sex": "m", "born": 1668, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"},
+  {"name": "Pieter Antone Haverbeke", "sex": "m", "born": 1753, "died": 1798, "father": "Jan Francies Haverbeke", "mother": "Petronella de Decker"},
+  {"name": "Maria van Brussel", "sex": "f", "born": 1801, "died": 1834, "father": "Jan Frans van Brussel", "mother": "Joanna de Causmaecker"},
+  {"name": "Angela Haverbeke", "sex": "f", "born": 1728, "died": 1734, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"},
+  {"name": "Elisabeth Haverbeke", "sex": "f", "born": 1711, "died": 1754, "father": "Jan Haverbeke", "mother": "Maria de Rycke"},
+  {"name": "Lievijne Jans", "sex": "f", "born": 1542, "died": 1582, "father":null, "mother":null},
+  {"name": "Bernardus de Causmaecker", "sex": "m", "born": 1721, "died": 1789, "father": "Lieven de Causmaecker", "mother": "Livina Haverbeke"},
+  {"name": "Jacoba Lammens", "sex": "f", "born": 1699, "died": 1740, "father": "Lieven Lammens", "mother": "Livina de Vrieze"},
+  {"name": "Pieter de Decker", "sex": "m", "born": 1705, "died": 1780, "father": "Joos de Decker", "mother": "Petronella van de Steene"},
+  {"name": "Joanna de Pape", "sex": "f", "born": 1654, "died": 1723, "father": "Vincent de Pape", "mother": "Petronella Wauters"},
+  {"name": "Daniel Haverbeke", "sex": "m", "born": 1652, "died": 1723, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"},
+  {"name": "Lieven Haverbeke", "sex": "m", "born": 1631, "died": 1676, "father": "Pieter Haverbeke", "mother": "Anna van Hecke"},
+  {"name": "Martina de Pape", "sex": "f", "born": 1666, "died": 1727, "father": "Vincent de Pape", "mother": "Petronella Wauters"},
+  {"name": "Jan Francies Haverbeke", "sex": "m", "born": 1725, "died": 1779, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"},
+  {"name": "Maria Haverbeke", "sex": "m", "born": 1905, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"},
+  {"name": "Petronella de Decker", "sex": "f", "born": 1731, "died": 1781, "father": "Pieter de Decker", "mother": "Livina Haverbeke"},
+  {"name": "Livina Sierens", "sex": "f", "born": 1761, "died": 1826, "father": "Jan Sierens", "mother": "Maria van Waes"},
+  {"name": "Laurentia Haverbeke", "sex": "f", "born": 1710, "died": 1786, "father": "Jan Haverbeke", "mother": "Maria de Rycke"},
+  {"name": "Carel Haverbeke", "sex": "m", "born": 1796, "died": 1837, "father": "Pieter Antone Haverbeke", "mother": "Livina Sierens"},
+  {"name": "Elisabeth Hercke", "sex": "f", "born": 1632, "died": 1674, "father": "Willem Hercke", "mother": "Margriet de Brabander"},
+  {"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"},
+  {"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"},
+  {"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"},
+  {"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}
+])
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+//Average function from the author
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+//byName object from the author
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+//my code - copied from the author :(
+var ageDifference = ancestry.filter(function(person){
+  return byName[person.mother] != null;
+}).map(function(person){
+  return person.born - byName[person.mother].born;
+});
+
+console.log(average(ageDifference));
+
+//Chapter 5, excercise 3, Historical Life Expectancy
+/*my attempt that just doesn't work
+var centuries = {};
+
+var deathCentury = ancestry.map(function(person){
+  return Math.ceil(person.died / 100);
+
+})
+
+forEach(deathCentury){
+  if(centuries.deathCentury != null){
+    //update existing array with person.(died-born)
+    centuries.deathCentury.push([deathCentury,person.born-person.died])
+  } else {
+    //update centuries with deathCentury and update that array with person.(died-born)
+    centuries.push(deathCentury[person.born-person.died])
+  })
+}*/
+
+//Author's answer with my comments to explain
+function groupBy(array, groupOf) { //creates a function called groupBy that takes an array and element called "groupOf"
+  var groups = {}; //creates an empty object called "groups"
+  array.forEach(function(element) { //pass every element in the array through a function - WHERE DOES ELEMENT COME FROM?
+    var groupName = groupOf(element); //creates a variable call "groupName" that is equal to an element of "groupOf"
+    if (groupName in groups) //if the "groupName" array exists in the object "groups"
+      groups[groupName].push(element); //then add the element to the "groupsName" array within the object "groups"
+    else //if the "groupName" array does not exist in the object "groups"
+      groups[groupName] = [element]; //the create a new "groups" array with the name "groupName" and set that equal to the element
+  });
+  return groups; //return all of the "groupName" arrays within the object "groups" - in this case, it should be the first two digits of each applicable century
+}
+
+var byCentury = groupBy(ancestry, function(person) { //creates a variable called "byCentury" and sets it equal to the Ancestry JSON object
+  return Math.ceil(person.died / 100); //grouped by death century for each person
+});
+
+for (var century in byCentury) { //WHERE DOES CENTURY COME FROM? for every "century" within the "byCentury" variable
+  var ages = byCentury[century].map(function(person) { //copy the Ancestry array (map) sorted "byCentury"
+    return person.died - person.born; //return the age of each person at death
+  });
+  console.log(century + ": " + average(ages)); //log the century with the average age of death
+}
+
+//Chapter 5, exercise 4, Every and Then Some
+function every(array,argument){
+  for(var i = 0; i <= array.length-1; i++){
+    if(array[i] === argument){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (!false){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(every([2,2,2], 2))
+
+function some(array,argument){
+  for(var i = 0; i <= array.length-1; i++){
+    if(array[i]===argument){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+console.log(some([6,3,1], 2))
